af4e2fa5ad355db9f2868362b8a6be05efe8c96d
Luca Previtali <lucaprevi@gmail.com>
2022-03-08 21:27:48 +0100

    minor refactor

--- src/Button.svelte
+++ src/Button.svelte
@@ -20,31 +20,13 @@
     });
   }
 
-  function applyVariant(variantName = "solid") {
-    switch (variantName) {
-      case "solid":
-        return solidVariant;
-
-      case "outline":
-        return outlineVariant;
-
-      case "link":
-        return linkVariant;
-
-      default:
-        return solidVariant;
-    }
-  }
-
   let rootStyle = "";
   $: rootStyle = `
     text-align: center;
     direction: ltr;
     border-radius: ${h / 6}px;
     user-select: none;
-    color: ${secondaryColor};
     font-weight: 600;
-    line-height: 1;
     padding: 10px 15px;
     cursor: pointer;
   `;
@@ -53,6 +35,7 @@
       background-color: ${primaryColor};
       color: ${secondaryColor};
       border-radius: ${h / 4}px;
+      border-color: ${primaryColor};
     `;
 
   $: outlineVariant = `
@@ -68,15 +51,23 @@
       border: none;
       text-decoration: underline;
     `;
+
+  let variants = {};
+
+  $: variants = {
+    solid: solidVariant,
+    outline: outlineVariant,
+    link: linkVariant,
+  };
 </script>
 
 <div class={containerClass} bind:clientHeight={h}>
   <button
     {id}
     {type}
-    class="svelte-button"
+    class={`svelte-button ${variant}`}
     on:click={disabled ? null : onClick}
-    style={`${rootStyle} ${applyVariant(variant)}`}
+    style={`${rootStyle} ${variants[variant]}`}
     {tabindex}
     {disabled}
   >
