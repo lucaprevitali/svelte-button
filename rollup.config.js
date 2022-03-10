4e8a5507a9120756b0b72337d0a832ac3de62529
Luca Previtali <lucaprevi@gmail.com>
2022-03-10 15:01:07 +0100

    changes in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
+import css from "rollup-plugin-css-only";
 
 const pkg = require("./package.json");
 
 export default {
   input: "src/components/Button.svelte",
   output: [
-    { file: pkg.module, format: "en" },
+    { file: pkg.module, format: "es" },
     { file: pkg.main, format: "umd", name: "Button" },
   ],
-  plugins: [svelte(), resolve()],
+  plugins: [css({ output: "style/vendor.css" }), svelte(), resolve()],
 };
